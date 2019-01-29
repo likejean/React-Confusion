3b3065a7c6ab68002d91a5c2788ce3807a33aa97
likejean <popachs@yahoo.com>
2019-01-29 03:16:34 -0800

    fixed error

--- src/components/MainComponent.js
+++ src/components/MainComponent.js
@@ -46,10 +46,10 @@ class Main extends Component {
                 <Header />
                 <Switch>
                     <Route path='/home' component={HomePage} />
-                    <Route exact path='/menu' component={() => <Menu dishes={this.state.dishes} />} />
+                    <Route exact path='/menu' component={() => <Menu dishes={this.props.dishes} />} />
                     <Route path='/menu/:dishId' component={DishWithId} />
                     <Route exact path='/contactus' component={Contact} />
-                    <Route exact path='/aboutus' component={() => <About leaders={this.state.leaders} />} />
+                    <Route exact path='/aboutus' component={() => <About leaders={this.props.leaders} />} />
                     <Redirect to='/home' />
                 </Switch>
                 <Footer />
