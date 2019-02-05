ac9ca43fc37e9aa7626de18f40f3641734135a24
likejean <popachs@yahoo.com>
2019-02-05 11:16:21 -0800

    Fetch

--- src/redux/ActionCreators.js
+++ src/redux/ActionCreators.js
@@ -1,6 +1,8 @@
 //ACTION
 import * as ActionTypes from './ActionTypes';
-import { DISHES } from '../shared/dishes';
+import { baseUrl } from '../shared/baseUrl';
+
+
 
 export const addComment = (dishId, rating, author, comment) => ({
     type: ActionTypes.ADD_COMMENT,
@@ -12,12 +14,14 @@ export const addComment = (dishId, rating, author, comment) => ({
     }
 });
 
+
+//1. DISHES.  Use fetcth() to get data from the server
 export const fetchDishes = () => (dispatch) => {
     dispatch(dishesLoading(true));
-
-    setTimeout(() => {
-        dispatch(addDishes(DISHES));
-    }, 2000);
+    
+    return fetch(baseUrl + 'dishes')
+        .then(response => response.json())
+        .then(dishes => dispatch(addDishes(dishes)));
 }
 
 export const dishesLoading = () => ({
@@ -27,9 +31,53 @@ export const dishesLoading = () => ({
 export const dishesFailed = (errmess) => ({
     type: ActionTypes.DISHES_FAILED,
     payload: errmess
-})
+});
 
 export const addDishes = (dishes) => ({
     type: ActionTypes.ADD_DISHES,
     payload: dishes
-})
+});
+
+
+
+//2.COMMENTS. Use fetch() to obtain Comments from database
+export const fetchComments = () => (dispatch) => {    
+    return fetch(baseUrl + 'comments')
+    .then(response => response.json())
+    .then(comments => dispatch(addComments(comments)));
+};
+
+export const commentsFailed = (errmess) => ({
+    type: ActionTypes.COMMENTS_FAILED,
+    payload: errmess
+});
+
+export const addComments = (comments) => ({
+    type: ActionTypes.ADD_COMMENTS,
+    payload: comments
+});
+
+//3. PROMOTIONS
+
+export const fetchPromos = () => (dispatch) => {
+    
+    dispatch(promosLoading());
+
+    return fetch(baseUrl + 'promotions')
+    .then(response => response.json())
+    .then(promos => dispatch(addPromos(promos)));
+}
+
+export const promosLoading = () => ({
+    type: ActionTypes.PROMOS_LOADING
+});
+
+export const promosFailed = (errmess) => ({
+    type: ActionTypes.PROMOS_FAILED,
+    payload: errmess
+});
+
+export const addPromos = (promos) => ({
+    type: ActionTypes.ADD_PROMOS,
+    payload: promos
+});
